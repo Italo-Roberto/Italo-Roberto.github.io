677d092b886bd1dd1e5ee7e3a19f672b2fdba9fb
Italo-Roberto <italoroberto61@gmail.com>
2022-06-24 10:43:17 -0300

    Commit inicial

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var numero = document.querySelector('#numero')
+var mensagem = document.querySelector('#mensagem')
+var link = document.querySelector('#link')
+
+function gerarLink() {
+    link.classList.add('link-show')
+    link.innerHTML = `https://api.whatsapp.com/send/?phone=55${numero.value}&text=${mensagem.value}&app_absent=0`
+}
+
+function copiarLink(){
+    navigator.clipboard.writeText(link.value)
+    .then(() => {
+        alert('Link copiado com sucesso! Abra uma nova aba de navegador e digite Crtl + V, para inicar uma conversa com esse link')
+    })
+}
+
+var atual = new Date()
+var ano = document.getElementById('ano')
+ano.innerHTML = atual.getFullYear()
