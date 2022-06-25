d16d7adc8030bc6d3ef01a6c2109ba9a07108d41
ITALO ROBERTO GOMES DE SOUZA <italo.r-souza@SEFAZ.PE.GOV.BR>
2022-06-25 14:08:06 -0300

    Realizado ajustes de Css e adicionado botão para oimpar campos

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var link = document.querySelector('#link')
 function gerarLink() {
     link.classList.add('link-show')
     link.innerHTML = `https://api.whatsapp.com/send/?phone=55${numero.value}&text=${mensagem.value}&app_absent=0`
+
 }
 
 function copiarLink(){
@@ -12,6 +13,12 @@ function copiarLink(){
     .then(() => {
         alert('Link copiado com sucesso! Abra uma nova aba de navegador e digite Crtl + V, para inicar uma conversa com esse link')
     })
+}
+
+function limparCampos() {
+    numero.value = ''
+    mensagem.value = ''
+    link.classList.remove('link-show')
 }
 
 var atual = new Date()
