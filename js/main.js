4775c72f4ead705444351b4699e7bbd5d73b2bee
ITALO ROBERTO GOMES DE SOUZA <italo.r-souza@SEFAZ.PE.GOV.BR>
2022-06-25 17:23:14 -0300

    Validação de campo numérico e defnindo comprimento do campo número

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ var mensagem = document.querySelector('#mensagem')
 var link = document.querySelector('#link')
 
 function gerarLink() {
-    if (numero.value != '' && mensagem.value != '') {
+    if(numero.value.length < 11 || isNaN(numero.value)){
+        alert('Digite um número de telefone no formato válido, ex: 81999999999 (ddd e número tudo junto sem parentêses ou acentos)')
+    }else if(numero.value != '' && mensagem.value != '') {
         link.classList.add('link-show')
         link.innerHTML = `https://api.whatsapp.com/send/?phone=55${numero.value}&text=${mensagem.value}&app_absent=0`
-    } else {
+    }else {
         alert('Necessário preencher campos de Número e Mensagem')
     }
 }
